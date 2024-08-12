cd5d6f830f5e38ddc05a87fc5be48c3555ce088c
ksulakshana <sulakshana146@gmail.com>
2024-08-12 10:56:42 +0530

    implemented routing

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './RegisterPage.module.css';
+
+function RegisterPage(props) {
+  return (
+    <div className={styles.container}>      
+        Register
+    </div>
+  )
+}
+
+export default RegisterPage
+
